b07a1ac935ee045cf8e8d2eb8fdb533a75f85758
VastavikRahul00 <150103487+VastavikRahul00@users.noreply.github.com>
2024-03-05 05:03:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iwhiwh/MY-SELF-BOT /MY-SELF-BOT
+  git clone https://github.com/VastavikRahul00/MY-SELF-BOT /MY-SELF-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MY-SELF-BOT
